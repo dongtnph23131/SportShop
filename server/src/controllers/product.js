68c15e385788e3cf594faffd34937c1ac9c083de
huy <havu6544@gmail.com>
2023-10-09 23:38:17 +0700

    feat: create product list and CRUD product

---
+++ server/src/controllers/product.js
@@ -0,0 +1,102 @@
+import Product from "../models/product";
+import { productValidators } from "../validators/product";
+
+export const getAll = async (req, res) => {
+    const { _limit = 10, _sort = "createAt", _order = "asc", _page = 1 } = req.query;
+    const options = {
+        page: _page,
+        limit: _limit,
+        sort: {
+            [_sort]: _order == "desc" ? -1 : 1,
+        },
+    };
+
+    try {
+        const data = await Product.paginate({}, options);
+
+        if (data.length == 0) {
+            return res.json({
+                message: "Không có sản phẩm nào",
+            });
+        }
+        return res.json(data);
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message,
+        });
+    }
+};
+export const get = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Product.findOne({ _id: id });
+        if (data.length === 0) {
+            return res.status(200).json({
+                message: "Không có sản phẩm",
+            });
+        }
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message,
+        });
+    }
+};
+export const create = async (req, res) => {
+    try {
+        const body = req.body;
+        const { error } = productValidators.validate(body);
+        if (error) {
+            return res.json({
+                message: error.details[0].message,
+            });
+        }
+        const product = await Product.create(body);
+        if (product.length === 0) {
+            return res.status(400).json({
+                message: "Thêm sản phẩm thất bại",
+            });
+        }
+        return res.status(200).json({
+            message: "Thêm sản phẩm thành công",
+            product,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message,
+        });
+    }
+};
+export const remove = async (req, res) => {
+    try {
+        const data = await Product.findByIdAndDelete(req.params.id);
+        return res.json({
+            message: "Xóa sản phẩm thành công",
+            data,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message,
+        });
+    }
+};
+export const update = async (req, res) => {
+    try {
+        const data = await Product.findOneAndUpdate({ _id: req.params.id }, req.body, {
+            new: true,
+        });
+        if (!data) {
+            return res.status(400).json({
+                message: "Cập nhật sản phẩm thất bại",
+            });
+        }
+        return res.json({
+            message: "Cập nhật sản phẩm thành công",
+            data,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message,
+        });
+    }
+};
