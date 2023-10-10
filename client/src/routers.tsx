ae2f16d625bafc012e42996843bab62e22578a43
ntbao10122003 <nguyenthebao1711@gmail.com>
2023-10-10 21:24:02 +0700

    Fetch:Ui-HomePage

--- client/src/routers.tsx
+++ client/src/routers.tsx
@@ -1,23 +1,24 @@
-import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "./components/layouts/LayoutAdmin";
-import { Navigate } from "react-router-dom";
-import Dashboard from "./components/pages/admin/Dashboard";
-import ProductPage from "./components/pages/admin/products/ProductPage";
-import AllCategories from "./components/pages/admin/categories/AllCategories";
-import ProductAdd from "./components/pages/admin/products/ProductAdd";
-import HomePage from "./components/pages/HomePage";
+import React from 'react'
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import WebsiteLayout from './components/WebsiteLayout';
+import Home from './components/Home';
+export const routers  = createBrowserRouter ([
+    {
+        path:"/",
+        element:<WebsiteLayout></WebsiteLayout>,
+        children: [
+            {index: true, element: <Navigate to="home"/>},
+            {
+                path: "home",
 
-export const routers = createBrowserRouter([
-  {path: "/", element: <HomePage/>},
-  {
-    path: "/admin",element: <MainLayout/>,
-    
-    children: [
-        {index: true, element: <Navigate to = "dashboard"/>},
-        {path: 'dashboard' , element: <Dashboard/>},
-        {path: 'products' , element: <ProductPage/>},   
-        {path: 'products/add' , element: <ProductAdd/>},
-        {path: 'categories', element: <AllCategories/>}
-    ]
-  },
-]);
+                element: (
+                    <>
+                        <Home></Home>
+                    </>
+                ),
+            },
+        ]
+
+    }
+
+ ])
