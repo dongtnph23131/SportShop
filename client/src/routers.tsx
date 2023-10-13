b4e2d15fc65b605282ace826b2c687942949bdce
manhld <manhthe21082003@gmail.com>
2023-10-13 21:34:42 +0700

    fix-router

--- client/src/routers.tsx
+++ client/src/routers.tsx
@@ -2,23 +2,27 @@ import React from 'react'
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import WebsiteLayout from './components/WebsiteLayout';
 import Home from './components/Home';
+import Cart from './components/Cart';
 export const routers  = createBrowserRouter ([
     {
         path:"/",
         element:<WebsiteLayout></WebsiteLayout>,
         children: [
-            {index: true, element: <Navigate to="home"/>},
+            {index: true, element: <Navigate to="Home"/>},
             {
-                path: "home",
+                path: "Home",
 
                 element: (
                     <>
                         <Home></Home>
                     </>
                 ),
+                
             },
+            {path: "/cart", element: <Cart/>}
         ]
 
-    }
+    },
+    
 
  ])
