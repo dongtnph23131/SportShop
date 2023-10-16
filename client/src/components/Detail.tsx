16afb2bf21e47e9d5823412f234d1b8c7a61ea62
ntbao10122003 <nguyenthebao1711@gmail.com>
2023-10-16 17:04:04 +0700

    fetch:ui-detail

---
+++ client/src/components/Detail.tsx
@@ -0,0 +1,239 @@
+import React from "react";
+
+const Detail = () => {
+  return (
+    <div>
+      <section id="prodetails" className="section-p1">
+        <div className="single-pro-image">
+          <img
+            src="../../src/Assets/product2.jpg"
+            width="100%"
+            id="MainImg"
+            alt=""
+          />
+
+          <div className="small-img-group">
+            <div className="small-img-col">
+              <img
+                src="../../src/Assets/product2.jpg"
+                width="100%"
+                className="small-img"
+                alt=""
+              />
+            </div>
+            <div className="small-img-col">
+              <img
+                src="../../src/Assets/product2.jpg"
+                width="100%"
+                className="small-img"
+                alt=""
+              />
+            </div>
+            <div className="small-img-col">
+              <img
+                src="../../src/Assets/product2.jpg"
+                width="100%"
+                className="small-img"
+                alt=""
+              />
+            </div>
+            <div className="small-img-col">
+              <img
+                src="../../src/Assets/product33.jpg"
+                width="100%"
+                className="small-img"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="single-pro-details">
+          <h6>Home / T-Shirts</h6>
+          <h4>Men's Fashion T Shirt</h4>
+          <h2> Price:$139.00</h2>
+          <div className="box-qlt">
+            <select>
+              <option>Select Size</option>
+              <option>XL</option>
+              <option>XXL</option>
+              <option>Small</option>
+              <option>Large</option>
+            </select>
+            <input type="number" value="1" />
+          </div>
+          <button className="normal">Add to Cart </button>
+          <h4>Product Details</h4>
+          <span>
+            The Gilden Ultra Cotton T-shirt is made from a substantial 6.0 oz.
+            per sq. yd. fabric constructed from 100% cotton, this classic fit
+            preshrunk jersey knit provides unmatched comfort with each wear.
+            Featuring a taped neck and shoulder, and a seamless double-needle
+            collar, and available in a range of colors, it offers it all in the
+            ultimate head-turning package.
+          </span>
+        </div>
+      </section>
+
+      <section id="product1" className="section-p1">
+        <h2>Featured Products</h2>
+        <p>Summer Collection New Modern Design</p>
+        <div className="pro-container">
+          <div className="pro">
+            <img src="../../src/Assets/product2.jpg" alt="" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut T-Shirts</h5>
+              <div className="star">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fab fa-opencart cart"></i>
+            </a>
+          </div>
+
+          <div className="pro">
+            <img src="../../src/Assets/product2.jpg" alt="" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut T-Shirts</h5>
+              <div className="star">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fab fa-opencart cart"></i>
+            </a>
+          </div>
+
+          <div className="pro">
+            <img src="../../src/Assets/product2.jpg" alt="" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut T-Shirts</h5>
+              <div className="star">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fab fa-opencart cart"></i>
+            </a>
+          </div>
+
+          <div className="pro">
+            <img src="../../src/Assets/product2.jpg" alt="" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut T-Shirts</h5>
+              <div className="star">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fab fa-opencart cart"></i>
+            </a>
+          </div>
+
+          <div className="pro">
+            <img src="../../src/Assets/product2.jpg" alt="" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut T-Shirts</h5>
+              <div className="star">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fab fa-opencart cart"></i>
+            </a>
+          </div>
+
+          <div className="pro">
+            <img src="../../src/Assets/product2.jpg" alt="" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut T-Shirts</h5>
+              <div className="star">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fab fa-opencart cart"></i>
+            </a>
+          </div>
+
+          <div className="pro">
+            <img src="../../src/Assets/product2.jpg" alt="" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Ladies paint</h5>
+              <div className="star">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fab fa-opencart cart"></i>
+            </a>
+          </div>
+
+          <div className="pro">
+            <img src="../../src/Assets/product2.jpg" alt="" />
+            <div className="des">
+              <span>adidas</span>
+              <h5>Cartoon Astronaut Dress</h5>
+              <div className="star">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+              </div>
+              <h4>$78</h4>
+            </div>
+            <a href="#">
+              <i className="fab fa-opencart cart"></i>
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Detail;
