330a14b873eb0c0c856ba8c892de3ea60f02055e
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2023-10-28 01:12:54 +0700

    fix:create-product-and-middlewares for Be

--- server/src/validators/product.js
+++ server/src/validators/product.js
@@ -7,27 +7,7 @@ export const productValidators = joi.object({
     }),
     categoryId: joi.string().required().messages({
         "string.empty": "Tên danh mục không được để trống",
-        "any.required": 'Trường tên là bắt buộc',
-    }),
-    sizes: joi.array().items(joi.string()).required().messages({
-        "string.empty": "Kích thước sản phẩm không được để trống",
-        "any.required": 'Trường kích thước là bắt buộc',
-    }),
-    colors: joi.array().items(joi.string()).required().messages({
-        "string.empty": "Màu sắc sản phẩm không được để trống",
-        "any.required": 'Trường màu sắc là bắt buộc',
-    }),
-    quantity: joi.number().required().messages({
-        "string.empty": "Số lượng sản phẩm không được để trống",
-        "any.required": "Trường số lượng là bắt buộc",
-    }),
-    brand: joi.string().required().messages({
-        "string.empty": "Thương hiệu không được để trống",
-        "any.required": "Trường thương hiệu là bắt buộc",
-    }),
-    price: joi.number().min(6).required().messages({
-        "string.empty": "Giá sản phẩm không được để trống",
-        "any.required": "Trường giá là bắt buộc",
+        "any.required": 'Trường tên danh mục là bắt buộc',
     }),
     description: joi.string().required().messages({
         "string.empty": "Mô tả không được để trống",
