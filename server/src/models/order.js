c0fbcd4ae791cd787985ccf6235b7e427159df25
Phuy29 <phuchuy292003@gmail.com>
2023-11-06 22:57:22 +0700

    add db schema

--- server/src/models/order.js
+++ server/src/models/order.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
+export const ORDER_STATUS = ["pending", "confirmed", "completed", "canceled"];
+
 const orderSchema = mongoose.Schema(
   {
     customerId: {
@@ -8,7 +10,43 @@ const orderSchema = mongoose.Schema(
       require: true,
       ref: "Customer",
     },
+    fullName: {
+      type: String,
+      require: true,
+    },
     email: {
+      type: String,
+      require: true,
+    },
+    phone: {
+      type: String,
+      require: true,
+    },
+    address: {
+      type: String,
+      require: true,
+    },
+    note: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ORDER_STATUS,
+      default: "pending",
+    },
+    shippingPrice: {
+      type: Number,
+      require: true,
+    },
+    totalPrice: {
+      type: Number,
+      require: true,
+    },
+    coupon_price: {
+      type: Number,
+      require: true,
+    },
+    type: {
       type: String,
     },
     items: [
